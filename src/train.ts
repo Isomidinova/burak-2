d479c90399811c725ad88a0d508b8cd18b52ea92
Shahnoza Isomidinova <shaxinozb@gmail.com>
2024-08-08 14:49:04 +0900

    fix: router admin naming according to project standards

--- src/train.ts
+++ src/train.ts
@@ -3,7 +3,7 @@ project standarts:
 - logging standarts
 - Naming standarts (misol: functions, method, variable => CAMEL case goHome)
 class => PASKAL         MemberService
-folder => KEBAB          
+folder, file => KEBAB          
 css => SNAKE             button_style
 
 -Error handling
